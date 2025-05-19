3dcbbe5a0770c594c7f466d532243d13ab566cdb
Your Name <you@example.com>
2025-05-19 09:47:05 +0200

    ajout du CRUD NOTE

--- src/Form/EtatForm.php
+++ src/Form/EtatForm.php
@@ -7,13 +7,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+
 class EtatForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nom')
-            ->add('couleur')
+            ->add('couleur', colorType::class, [
+			'label' =>'couleur',
+			'html5' =>true,
+			'required' =>true
+			])
         ;
     }
 
